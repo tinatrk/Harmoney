b52a3b1998e71bf4078cb866dd9756330ceffc30
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2026-03-25 16:17:21 +0300

    A little correction of HarmColor.kt

--- app/src/main/java/com/example/harmoney/ui/theme/HarmColor.kt
+++ app/src/main/java/com/example/harmoney/ui/theme/HarmColor.kt
@@ -27,7 +27,7 @@ import androidx.compose.ui.graphics.Color
  * **Common**
  * - `Purple100` - primary_variant
  * - `White` - on_primary
- * * - `Red400` - error
+ * - `Red400` - error
  * - `Red50` - error_container
  * - `Green600` - info
  *
@@ -38,15 +38,14 @@ object HarmColor {
     val Purple50 = Color(0xFFF3E5F5)
     val Gray900 = Color(0xFF1C1B1F)
     val Gray600 = Color(0xFF757575)
+    val Purple400 = Color(0xFF8b22b6)
 
     val Gray800 = Color(0xFF49454F)
     val White = Color(0xFFFFFFFF)
     val Gray400 = Color(0xFFCAC4D0)
-
-    val Purple400 = Color(0xFF8b22b6)
     val Purple800 = Color(0xFF5d1a99)
-    val Purple100 = Color(0xFFF7DAFC)
 
+    val Purple100 = Color(0xFFF7DAFC)
     val Red400 = Color(0xFFED405B)
     val Red50 = Color(0xFFFDEBEE)
     val Green600 = Color(0xFF18A75A)
