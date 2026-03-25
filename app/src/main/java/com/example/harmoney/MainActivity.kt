110df805c1e06ea54725ecedce61dc212d30781b
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2026-03-25 15:42:39 +0300

    Add empty screen placeholder

--- app/src/main/java/com/example/harmoney/MainActivity.kt
+++ app/src/main/java/com/example/harmoney/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.harmoney.ui.theme.HarmTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,11 +44,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        color = HarmTheme.colors.onSurface
-    )
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hello $name!",
+            color = HarmTheme.colors.onSurface
+        )
+        Image(
+            modifier = Modifier
+                .padding(horizontal = 24.dp, vertical = 24.dp)
+                .fillMaxWidth(),
+            painter = painterResource(R.drawable.im_empty_screen),
+            contentDescription = null,
+            contentScale = ContentScale.Fit
+        )
+    }
 }
 
 @Preview(showBackground = true)
