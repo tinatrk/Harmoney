6c796d9dcb825018da5e7ade174cdef29ae69df9
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2026-03-31 18:02:36 +0300

    Add circular iconButtons to the uilibrary

--- app/src/main/java/com/example/harmoney/core/uilibrary/buttons/HarmButton.kt
+++ app/src/main/java/com/example/harmoney/core/uilibrary/buttons/HarmButton.kt
@@ -1,17 +1,32 @@
 package com.example.harmoney.core.uilibrary.buttons
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.harmoney.presentation.category.models.CategoryColors
+import com.example.harmoney.ui.theme.HarmColor
 import com.example.harmoney.ui.theme.HarmTheme
 
 object HarmButton {
@@ -56,6 +71,85 @@ object HarmButton {
             Icon(
                 painter = painterResource(iconRes),
                 contentDescription = contentDescription
+            )
+        }
+    }
+
+    /** Circular iconButton */
+    @Composable
+    fun HarmCircularIconButton(
+        onClick: () -> Unit,
+        @DrawableRes iconRes: Int?,
+        contentDescription: String?,
+        modifier: Modifier = Modifier,
+        iconBackground: Color = CategoryColors.CyanT88.color,
+        selected: Boolean = false,
+    ) {
+        Box(
+            modifier = modifier.size(48.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .clickable { onClick() }
+                    .background(iconBackground)
+                    .then(
+                        if (selected) {
+                            Modifier.border(
+                                width = 2.dp,
+                                color = HarmTheme.colors.primary,
+                                shape = CircleShape
+                            )
+                        } else {
+                            Modifier
+                        }
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                if (iconRes != null) {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(iconRes),
+                        contentDescription = contentDescription,
+                        tint = HarmColor.NeutralT0
+                    )
+                }
+            }
+        }
+    }
+
+    /** Circular iconButton with title*/
+    @Composable
+    fun HarmCircularIconButtonWithTitle(
+        @DrawableRes iconRes: Int,
+        iconBackground: Color,
+        iconTitle: String,
+        onClick: () -> Unit,
+        contentDescription: String?,
+        modifier: Modifier = Modifier,
+        selected: Boolean = false,
+    ) {
+        Column(
+            modifier = modifier.padding(4.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            HarmCircularIconButton(
+                iconRes = iconRes,
+                iconBackground = iconBackground,
+                onClick = onClick,
+                contentDescription = contentDescription,
+                selected = selected
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = iconTitle,
+                color = if (selected) {
+                    HarmTheme.colors.primary
+                } else HarmTheme.colors.onSurfaceContainer,
+                style = HarmTheme.typography.labelLarge
             )
         }
     }
--- app/src/main/java/com/example/harmoney/presentation/category/models/CategoryColors.kt
+++ app/src/main/java/com/example/harmoney/presentation/category/models/CategoryColors.kt
@@ -2,22 +2,46 @@ package com.example.harmoney.presentation.category.models
 
 import androidx.compose.ui.graphics.Color
 
-object CategoryColors {
-    val Pink100 = Color(0xFFF8BBD0)
-    val Red100 = Color(0xFFFFCDD2)
-    val Red100_2 = Color(0xFFFFCCBC)
-    val Orange100 = Color(0xFFFFE0B2)
-    val Orange100_2 = Color(0xFFFFECB3)
-    val Yellow100 = Color(0xFFFFECB3)
-    val Yellow100_2 = Color(0xFFFFF9C4)
-    val Yellow200 = Color(0xFFE6EE9C)
-    val Green100 = Color(0xFFDCEDC8)
-    val Green100_2 = Color(0xFFC8E6C9)
-    val Blue100 = Color(0xFFB2EBF2)
-    val Blue100_2 = Color(0xFFB3E5FC)
-    val Blue100_3 = Color(0xFFC5CAE9)
-    val Purple200 = Color(0xFFB39DDB)
-    val Purple100 = Color(0xFFE1BEE7)
-    val Gray100_1 = Color(0xFFD7CCC8)
-    val Gray100_2 = Color(0xFFCFD8DC)
+enum class CategoryColors(val color: Color) {
+    RedT53(Color(0xFFE13F36)),
+    PinkT58(Color(0xFFEC407B)),
+    PinkT65(Color(0xFFF06292)),
+    RedT68(Color(0xFFE57373)),
+    RedT77(Color(0xFFEFA0AC)),
+    PinkT83(Color(0xFFF5BEDC)),
+
+    OrangeT47(Color(0xFFC5881D)),
+    OrangeT55(Color(0xFFF0652D)),
+    YellowT58(Color(0xFFD4B36A)),
+    OrangeT68(Color(0xFFFF8A65)),
+    OrangeT74(Color(0xFFFF9E80)),
+    OrangeT78(Color(0xFFFFB38A)),
+    YellowT82(Color(0xFFFFCA5F)),
+    OrangeT85(Color(0xFFFFCCBC)),
+    OrangeT90(Color(0xFFFFE0B2)),
+
+    GreenT53(Color(0xFF348D68)),
+    TealT57(Color(0xFF009688)),
+    LimeT62(Color(0xFF9EBA57)),
+    GreenT70(Color(0xFF5DBB8B)),
+    GreenT88(Color(0xFFC8E6C9)),
+    LimeT89(Color(0xFFE6EE9C)),
+
+    IndigoT60(Color(0xFF7986CB)),
+    BlueT60(Color(0xFF2196F3)),
+    BlueT62(Color(0xFF6F99AD)),
+    BlueT75(Color(0xFF64B5F6)),
+    IndigoT83(Color(0xFFC5CAE9)),
+    CyanT88(Color(0xFFB3E5FC)),
+
+    MagentaT42(Color(0xFFB500A4)),
+    PurpleT61(Color(0xFFBA68C8)),
+    VioletT68(Color(0xFFB085F5)),
+    PurpleT71(Color(0xFFB39DDB)),
+    PurpleT72(Color(0xFFC39BD3)),
+    PurpleT84(Color(0xFFE1BEE7)),
+
+    BrownT51(Color(0xFFA26B6D)),
+    BrownT82(Color(0xFFD7CCC8)),
+    BlueGrayT85(Color(0xFFCFD8DC)),
 }
