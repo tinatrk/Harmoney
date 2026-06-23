e21a867a9bcb2f284dc51799a0334a8b55106e39
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2026-06-23 16:36:33 +0300

    Add needed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.devtoolsKsp) apply false
 }
